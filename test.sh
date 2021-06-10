b30362d04fa800822c4d24a300589068c0192ae8
Patric Jansson <paddy@kth.se>
2021-06-10 14:11:05 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Testing how Github Actions handles secrets.
+#
+# MULTI_LINE_3_VARS
+# A=1 B=2
+# C=3
+#
+# PER_LINE_3_VARS
+# A=1
+# B=2
+# C=3
+#
+# ONE_LINE_3_VARS
+# A=1 B=2 C=3
+
 echo "Hello world"
 
 echo "${SUPER_SECRET}" >  file.txt
@@ -7,3 +21,7 @@ echo "${SUPER_SECRET}" >  file.txt
 cat file.txt
 
 wc -l file.txt
+
+echo "A: $A"
+echo "B: $B"
+echo "C: $C"
