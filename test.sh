9c8f36038274d4072f4fea09e73bd7596ec7ed41
Patric Jansson <paddy@kth.se>
2021-06-10 14:15:39 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Testing how Github Actions handles secrets.
-#
 # MULTI_LINE_3_VARS
 # A=1 B=2
 # C=3
@@ -14,14 +12,21 @@
 # ONE_LINE_3_VARS
 # A=1 B=2 C=3
 
-echo "Hello world"
+echo "Testing how Github Actions handles secrets"
 
 VARS=${SUPER_SECRET}
-echo "${SUPER_SECRET}" >  file.txt
+echo "Chars:"
+wc -c $VARS
 
+echo "--words file ------- "
+echo "${VARS}" >  file.txt
 cat file.txt
-
 wc -l file.txt
+
+echo "--words file2 ------- "
+echo "${SUPER_SECRET}" >  file2.txt
+cat file2.txt
+wc -l file2.txt
 
 echo "A: $A"
 echo "B: $B"
